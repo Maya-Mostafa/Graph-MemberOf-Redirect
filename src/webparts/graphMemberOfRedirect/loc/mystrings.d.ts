e68f1b531a5b9d1841f3731e8a196e541589d208
Maya-Mostafa <mayamostafa@gmail.com>
2020-11-23 14:29:53 -0500

    first commit

---
+++ src/webparts/graphMemberOfRedirect/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGraphMemberOfRedirectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GraphMemberOfRedirectWebPartStrings' {
+  const strings: IGraphMemberOfRedirectWebPartStrings;
+  export = strings;
+}
